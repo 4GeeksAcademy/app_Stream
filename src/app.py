7c77b1ff67e7ed0b0cf72858cf3bd5d6e88f2d06
emilceinfante <emilceinfante@gmail.com>
2025-07-12 22:36:16 +0000

    avance

--- src/app.py
+++ src/app.py
@@ -1,4 +1,24 @@
-from utils import db_connect
-engine = db_connect()
+from pickle import load
+import streamlit as st
 
-# your code here
+model = load(open("../models/random_forest_classifier_default_42.sav", "rb"))
+class_dict = {
+    "0": "Introvertido",
+    "1": "Extrovertido",
+    
+}
+
+st.title("Personalidad - Model prediction")
+
+val1 = st.slider("Apertura a nuevas experiencias", min_value = 1, max_value = 5, step = 1)
+val2 = st.slider("Responsabilidad", min_value = 1, max_value = 5, step = 1)
+val3 = st.slider("Extroversión", min_value = 1, max_value = 5, step = 1)
+val4 = st.slider("Amabilidad", min_value = 1, max_value = 5, step = 1)
+val5 = st.slider("Estabilidad emocional", min_value = 1, max_value = 1, step = 1)
+val6 = st.slider("Habilidades sociales", min_value = 1, max_value = 5, step = 1)
+val7 = st.slider("Energía y actividad", min_value = 1, max_value = 5, step = 1)
+
+if st.button("Predict"):
+    prediction = str(model.predict([[val1, val2, val3, val4, val5, val6, val7]])[0])
+    pred_class = class_dict[prediction]
+    st.write("Prediction:", pred_class)
