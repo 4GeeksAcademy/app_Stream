f8cadd997dedd4812b3f521572b766a46a08ce17
emilceinfante <emilceinfante@gmail.com>
2025-07-24 14:14:43 +0000

    avance

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 from pickle import load
 import streamlit as st
 
-model = load(open("../models/RandomForestClassifier_default_42.sav", "rb"))
+model = load(open("./models/RandomForestClassifier_default_42.sav", "rb"))
 class_dict = {
-    "0": "Extrovertido",
-    "1": "Introvertido",
+    "0": "Introvertido",
+    "1": "Extrovertido",
     
 }
 
@@ -14,11 +14,18 @@ val1 = st.slider("Apertura a nuevas experiencias", min_value = 1, max_value = 5,
 val2 = st.slider("Responsabilidad", min_value = 1, max_value = 5, step = 1)
 val3 = st.slider("Extroversión", min_value = 1, max_value = 5, step = 1)
 val4 = st.slider("Amabilidad", min_value = 1, max_value = 5, step = 1)
-val5 = st.slider("Estabilidad emocional", min_value = 1, max_value = 1, step = 1)
+val5 = st.slider("Estabilidad emocional", min_value = 0, max_value = 1, step = 1)
 val6 = st.slider("Habilidades sociales", min_value = 1, max_value = 5, step = 1)
 val7 = st.slider("Energía y actividad", min_value = 1, max_value = 5, step = 1)
 
 if st.button("Predict"):
     prediction = str(model.predict([[val1, val2, val3, val4, val5, val6, val7]])[0])
     pred_class = class_dict[prediction]
+    st.write("Prediction:", pred_class)
+
+if st.button("Predict"):
+    raw_prediction = model.predict([[val1, val2, val3, val4, val5, val6, val7]])
+    prediction = str(raw_prediction[0])
+    pred_class = class_dict[prediction]
+    st.write("Raw Prediction:", raw_prediction)  # Imprimir la predicción cruda
     st.write("Prediction:", pred_class)
