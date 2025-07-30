04521ae1a58cd1323cac2992ada8c37c95e97c5a
emilceinfante <emilceinfante@gmail.com>
2025-07-30 16:57:24 +0000

    app

--- src/app.py
+++ src/app.py
@@ -3,21 +3,21 @@ import streamlit as st
 from joblib import load
 
 # Cargar el modelo desde el archivo
-model = load("../models/random_forest_regressor_default_42.sav")
+model = load("./models/random_forest_regressor_default_42.sav")
 print("✅ Modelo cargado exitosamente!")
 
 # Título de la aplicación
 st.title("HIV - Predicción del Modelo")
 
 # Ingreso de edad
-val1 = st.slider("Sexo", min_value=18, max_value=22, step=1)
+val1 = st.slider("Sexo", min_value=18, max_value=60, step=1)
 
 # Ingreso de género
-val2 = st.slider("Año", min_value=0, max_value=1, step=1)
+val2 = st.slider("Año", min_value=2019, max_value=2023, step=1)
 
 
 # Ingreso de examen de admisión
-val3 = st.slider("Grupo Etario", min_value=6, max_value=10, step=1)
+val3 = st.slider("Grupo Etario", min_value=1, max_value=9, step=1)
 
 
 # Botón para realizar la predicción
